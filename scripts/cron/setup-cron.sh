62ed05965413ecb184c030e136e4084bc90d1570
Danya <danyakulazhenko835@gmail.com>
2026-01-17 23:54:21 +0200

    disable cron

--- scripts/cron/setup-cron.sh
+++ scripts/cron/setup-cron.sh
@@ -4,7 +4,7 @@ apk add --no-cache curl
 
 crontab -r || true
 
-echo "0 0 * * * curl -X POST -H \"Authorization: Bearer $CRON_SECRET\" \"$APP_BASE_URL/api/cron/external-sources\" >> /var/log/cron.log 2>&1" >> /etc/crontabs/root
+#echo "0 0 * * * curl -X POST -H \"Authorization: Bearer $CRON_SECRET\" \"$APP_BASE_URL/api/cron/external-sources\" >> /var/log/cron.log 2>&1" >> /etc/crontabs/root
 
 touch /var/log/cron.log
 
